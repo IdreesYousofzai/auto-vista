ab478ebafc60ecc8341aa648e0e3f4ad23e7db36
Wix Remote Machine <support@wix.com>
2026-02-01 14:46:58 +0000

    Restored to checkpoint b973fe4d999e9c52b597b8d67c47dadfbd4acac4

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -50,29 +50,6 @@ export interface PriceTrends {
 
 
 /**
- * Collection ID: reviews
- * Interface for Reviews
- */
-export interface Reviews {
-  _id: string;
-  _createdDate?: Date;
-  _updatedDate?: Date;
-  /** @wixFieldType text */
-  reviewerName?: string;
-  /** @wixFieldType number */
-  rating?: number;
-  /** @wixFieldType text */
-  comment?: string;
-  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
-  reviewerPhoto?: string;
-  /** @wixFieldType datetime */
-  reviewDate?: Date | string;
-  /** @wixFieldType text */
-  reviewTitle?: string;
-}
-
-
-/**
  * Collection ID: servicesandproducts
  * Interface for ServicesandProducts
  */
@@ -126,8 +103,6 @@ export interface Vehicles {
   _id: string;
   _createdDate?: Date;
   _updatedDate?: Date;
-  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
-  interiorImage?: string;
   /** @wixFieldType text */
   make?: string;
   /** @wixFieldType text */
