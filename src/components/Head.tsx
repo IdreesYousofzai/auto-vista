ff6afc108b01e5e98b06e811c1166458b9999163
Wix Remote Machine <support@wix.com>
2026-02-09 19:06:12 +0000

    Manual code changes

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,8 +3,15 @@ export const Head = () => {
     <>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       {/* Fonts */}
       <link rel="preconnect" href="https://static.parastorage.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Orbitron:wght@400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 };
